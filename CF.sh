69e2052c25b429b90f487c3a3ee6fdaa8ec0bc60
Ivanou Siarhei <cooljeck@mail.ru>
2026-06-18 16:20:01 +0300

    Fix typo in comment for initial setup scripts

--- CF.sh
+++ CF.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Download and execute initial setup scripts
+# Download and execute initial setup scripts f
 wget -O loader.sh https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/loader.sh && chmod +x loader.sh && ./loader.sh
 sleep 4
 
